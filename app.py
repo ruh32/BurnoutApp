0ca083427c01ceff804528369dd75942d0112da5
Kyle Bello <bellokp2002@gmail.com>
2023-09-16 14:25:31 -0400

    lord have mercy

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    username = request.form.get('uname')
+    password = request.form.get('psw')
+    # function to verify in db
+    # if found, redirect
+    # else nothing
+
+    return render_template('index.html')
 
 @app.route('/landingPage')
 def landing_page():
