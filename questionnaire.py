8127b3be1b17c4da7fcd5e528062cf9ff466bea6
Kyle Bello <bellokp2002@gmail.com>
2023-09-16 01:14:53 -0400

    created questionnaire class

--- questionnaire.py
+++ questionnaire.py
@@ -1,3 +1,15 @@
-class questionnaire:
-    def __init__(
+class Questionnaire:
     
+    def __init__(self, id, date, question_responses):
+        self.id = id
+        self.date = date
+        self.respones = question_responses
+    
+    def get_id(self):
+        return self.id
+    
+    def get_date(self):
+        return self.date
+    
+    def get_question_response(self, i):
+        return self.responses[i]
